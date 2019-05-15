f38cb6b2137fa414ea5e26f46559eb039119d227
Ben Parisot <me@benparisot.com>
2019-05-15 15:30:16 -0700

    wrote Photos component

---
+++ src/components/Photos.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Photo from './Photo';
+import PropTypes from 'prop-types';
+
+export default function Photos({ photos }) {
+  const photoList = photos.map(photo => {
+    return <li key={photo}><Photo photo={photo.photo} /></li>;
+  });
+
+  return (
+    <ul>
+      {photoList}
+    </ul>
+  );
+}
+
+Photos.propTypes = {
+  photos: PropTypes.array.isRequired
+};
