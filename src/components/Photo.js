a5c04ebde6e6865264a132beca0ad0265879bca2
Ben Parisot <me@benparisot.com>
2019-05-15 15:22:55 -0700

    write Photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+export default function Photo({ photo }) {
+  return (
+    <img src={photo}></img>
+  );
+} 
+
+Photo.propTypes = {
+  photo: PropTypes.string.isRequired
+};
