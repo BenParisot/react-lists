3eac56683f84645d536ab68ccf13cac5d1b523eb
Ben Parisot <me@benparisot.com>
2019-05-15 15:02:02 -0700

    write App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Colors from './Colors';
+import PhotoAlbum from './PhotoAlbum';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function App() {
-  const colors = [
-    { name: 'red', hex: '#ff0000', rgb: { red: 255, green: 0, blue: 0 } },
-    { name: 'green', hex: '#00ff00', rgb: { red: 0, green: 255, blue: 0 } },
-    { name: 'blue', hex: '#0000ff', rgb: { red: 0, green: 0, blue: 255 } }
+  const title = 'My Photos';
+  const photos = [
+    { },
+    { },
+    { }
   ];
-  return <Colors colors={colors} />;
+  return (
+    <>
+      <Header />
+      <PhotoAlbum title={title} photos={photos} />
+      <Footer />
+    </>
+  );
 
 }
